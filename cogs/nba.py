a7855b90fb49f78c840ced2c41abbf08eedd5d10
Steve Hiatt <schwa@xmission.com>
2023-01-02 13:13:50 -0700

    add /stats command

--- cogs/nba.py
+++ cogs/nba.py
@@ -207,6 +207,47 @@ class nba(commands.Cog):
         print(str("\n".join(stats)))
         await interaction.followup.send(str("\n".join(stats)))
 
+    @discord.app_commands.command(name="stats", description="Player season stats")
+    @discord.app_commands.describe(playername="An NBA player's name")
+    async def nbastats(
+        self, interaction: discord.Interaction, playername: str, games: int = 1
+    ):
+        # """List an NBA player's statline from the most recent game"""
+        await interaction.response.defer()
+        try:
+            player = players.find_players_by_full_name(playername)
+            assert len(player) > 0
+            player_id = player[0]["id"]
+            player_info = commonplayerinfo.CommonPlayerInfo(
+                player_id
+            ).common_player_info.get_dict()
+        except:
+            await interaction.followup.send(f"Player not found, {playername}")
+        stats = [
+            f"Recent Stats for {player[0]['full_name']} ({player_info['data'][0][20]})\n"
+        ]
+        info = PlayerDashboardByGameSplits(
+            last_n_games=games,
+            month=0,
+            opponent_team_id=0,
+            pace_adjust="N",
+            per_mode_detailed="PerGame",
+            period=0,
+            player_id=player_id,
+            season="2022-23",
+        ).get_dict()["resultSets"][0]["rowSet"][0]
+        stats.append(
+            f"{info[26]} PTS  {info[18]}/{info[16]} RB  {info[19]} AS  {info[22]} BL  {info[21]} ST"
+        )
+        stats.append(
+            f"{str(info[9]).lstrip('0')} of {(info[8])} FG / {str(info[12]).lstrip('0')} of {info[11]} 3P / {str(info[15]).lstrip('0')} of {info[14]} FT"
+        )
+        stats.append(
+            f"{info[20]} TO  {info[24]} PF  {info[27]} +/-  {info[6]} MN  {info[2]} GP"
+        )
+        print(str("\n".join(stats)))
+        await interaction.followup.send(str("\n".join(stats)))
+
     @discord.app_commands.command(
         name="leaders", description="The top 10 players in a given statistical category"
     )
